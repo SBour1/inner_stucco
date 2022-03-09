d03bf55e98b73e371a523920e4042b07034230c1
Titus Knox <knoxtitus@gmail.com>
2022-03-09 12:07:06 -0500

    added homeRotues.js and loginRoutes.js

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,8 @@
+const router = require('express').router();
+
+router.get('/', async (req,res) => {
+    res.render(''); 
+    //The homepage handlebar will go the parentheses ()
+});
+
+modeule.exports = router;
