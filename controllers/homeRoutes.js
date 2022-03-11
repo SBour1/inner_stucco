248f2a2458877324f6d9c600e3e409c22e3b64a2
Titus Knox <knoxtitus@gmail.com>
2022-03-10 20:35:14 -0500

    added menu.js w routes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,21 +1,33 @@
-const router = require('express').router();
+const router = require('express').Router();
+const { User } = require('../models');
+const withAuth = require('../utils/auth');
 
-router.get('/', async (req,res) => {
-    res.render('homepage'); 
-    
-}); // Homepage route
+router.get('/', withAuth, async (req, res) => {
+    try {
+        const userData = await User.findAll({
+            attributes: { exclude: ['password'] },
+            order: [['name', 'ASC']],
+    });
 
-router.get('/about', async (req,res) => {
-    res.render('about');
+        const users = userData.map((project) => project.get({ plain: true }));
 
+    res.render('homepage', {
+        users,
+        logged_in: req.session.logged_in,
+    });
+    } catch (err) {
+    res.status(500).json(err);
+    }
 });
 
-router.get('/menu', async (req,res) => {
-    res.render('menu');
+router.get('/login', (req, res) => {
+    if (req.session.logged_in) {
+        res.redirect('/');
+        return;
+    }
+
+    res.render('login');
 });
 
-router.get('/order', async (req,res) => {
-    res.render('order');
-});
 
-modeule.exports = router;
+module.exports = router;
--- controllers/menu.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').router(); 
-
-router.get('/menu', (req,res) => {
-    res.render('menuitems');
-}) 
-
-router.get('/menu/:id', (req,res) => {
-    
-})
