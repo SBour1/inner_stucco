dbd4609b920df998a3ae8165807e988efffb9fd8
Radhika <cu9rad@gmail.com>
2022-03-17 19:41:33 -0400

    made changes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,16 +3,12 @@ const loginRoute = require('./loginRoute');
 const menuRoute = require('./menuroute');
 const orderRoute = require('./orderRoute');
 const signupRoute = require('./signupRoute');
+const userRoutes = require('./user-routes');
 
 router.use('/login', loginRoute);
 router.use('/menu', menuRoute);
 router.use('/order', orderRoute);
 router.use('/signup', signupRoute);
-
-
-const userRoutes = require('./user-routes');
-
 router.use('/users', userRoutes);
-
 
 module.exports = router;
